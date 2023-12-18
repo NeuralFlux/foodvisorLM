90039706789384c98fb42d08724506af9a2a0954
NeuralFlux <40491005+NeuralFlux@users.noreply.github.com>
2023-12-17 21:30:51 -0500

    fixed login and logout issues, formatted history page

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,5 +1,7 @@
+import os
 import json
 import requests
+from urllib import parse
 import pprint
 
 from functools import wraps
@@ -28,67 +30,46 @@ def login_required(f):
 
 @app.route('/')
 def index():
-    return render_template("pages/index.html")
+    return render_template("pages/index.html", login_missing=True)
 
 @app.route('/login')
 def login():
-    print("login")
-    print(request.args)
     if "code" in request.args:
         session["sid"] = request.args.get("code")
         return redirect(url_for('search'))
     else:
-        from urllib.parse import urlparse, quote
-        o = urlparse(request.base_url)
-        
-        host = o.hostname
-        if host == "localhost":
-            host = f"http://{host}:5000"
-        else:
-            host = "https://" + host
-
-        safe_redirect_url = quote(f"https://{host}{url_for('login')}", safe="")
-        return redirect(f"https://foodvisor-lm.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=4fsa9c9emuj8up23oekojflnt8&response_type=code&scope=email+openid&redirect_uri={safe_redirect_url}")
+        safe_redirect_url = parse.quote(request.url, safe="")
+        return redirect(f"https://foodvisor-lm.auth.us-east-1.amazoncognito.com/login?client_id=4fsa9c9emuj8up23oekojflnt8&response_type=code&scope=email+openid&redirect_uri={safe_redirect_url}")
 
 
-@login_required
 @app.route('/logout')
-def logout():
-    return redirect(url_for('index'))
-    try:
-        session.pop("sid")
-    except:
-        pass
-
-    from urllib.parse import urlparse, quote
-    o = urlparse(request.base_url)
-    
-    host = o.hostname
-    if host == "localhost":
-        host = f"http://{host}:5000"
-    else:
-        host = "https://" + host
-
-    safe_redirect_url = quote(f"https://{host}{url_for('index')}", safe="")
-    return redirect(f"https://foodvisor-lm.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=4fsa9c9emuj8up23oekojflnt8&response_type=code&scope=email+openid&logout_uri={safe_redirect_url}")
-
-
-
 @login_required
-@app.route('/history')
-def history():
-    try:
-        resp = requests.get(f"https://wnpwytxwol.execute-api.us-east-1.amazonaws.com/v1/history")
-        history = resp.json()["body-json"]["Item"]["last_one"]
-    except:
-        history = None
+def logout():
+    print("Logging out")
+    if "sid" in session.keys():
+        print("session present")
+        session.pop("sid")
 
-    print(history, resp.content)
+        parsed_url = parse.urlparse(request.url)
+        safe_redirect_url = parse.quote(
+            f"{parsed_url.scheme}://{parsed_url.netloc}{url_for('index')}",
+            safe=""
+        )
+        return redirect(f"https://foodvisor-lm.auth.us-east-1.amazoncognito.com/logout?client_id=4fsa9c9emuj8up23oekojflnt8&logout_uri={safe_redirect_url}")
+
+
+
+@app.route('/history')
+@login_required
+def history():
+    resp = requests.get(f"https://wnpwytxwol.execute-api.us-east-1.amazonaws.com/v1/history")
+    history = resp.json()["body-json"]["Items"]
+
     return render_template('pages/history.html', history=history)
 
 
-@login_required
 @app.route('/search', methods=["GET", "POST"])
+@login_required
 def search():
     form = forms.GTINForm(request.form)
     if request.form is not None:
