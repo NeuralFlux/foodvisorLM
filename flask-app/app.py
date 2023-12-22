9e10f7be1fba5af47a37746b3e5a50646c35ccfd
NeuralFlux <40491005+NeuralFlux@users.noreply.github.com>
2023-12-22 01:52:29 -0500

    added extra attributes to history and search, rendered relevant attributes in templates, made templates look better

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,6 +3,7 @@ import json
 import requests
 from urllib import parse
 import time
+from datetime import datetime
 
 from functools import wraps
 from flask import Flask, flash, render_template, request, session, url_for, redirect
@@ -40,6 +41,7 @@ def index():
 @app.route('/login')
 def login():
     if "code" in request.args:
+        # TODO modularize
         auth_code = request.args.get("code")
         url = "https://foodvisor-lm.auth.us-east-1.amazoncognito.com/oauth2/token"
 
@@ -115,7 +117,9 @@ def history():
                 history[idx][key] = history[idx][key][dtype]
             if key == "stringified_labels_json":
                 # FIXME json string alternatives
-                history[idx][key] = json.loads(history[idx][key].replace("'", "\""))
+                history[idx][key] = json.loads(parse.unquote(history[idx][key]).replace("'", "\""))
+            elif key == "created_at":
+                history[idx][key] = datetime.fromtimestamp(int(history[idx][key])).strftime('%H:%M:%S on %d %B, %Y')
 
     return render_template('pages/history.html', history=history)
 
@@ -128,16 +132,30 @@ def search():
     form = forms.GTINForm(request.form)
     if request.form is not None:
         if form.validate_on_submit():
-            b64_str = form.barcode_image.data
-            barcode = bcode_reader.detect_bcode(b64_str)
+            gtin_upc = form.barcode_val.data
 
-            if barcode is None:
-                flash("Invalid barcode, please try again")
-                return redirect(url_for('search'))
-            gtin_upc = int(barcode.rjust(14, "0"))
+            # look for barcode only if number is not present
+            if gtin_upc is None:
+                b64_str = form.barcode_image.data
+                barcode = bcode_reader.detect_bcode(b64_str)
 
-            resp = requests.get(f"https://wnpwytxwol.execute-api.us-east-1.amazonaws.com/v1/gtin_table?gtin_upc={gtin_upc}")
-            ingredients = resp.json()["body-json"]["Item"]["ingredients"]["S"]
+                if barcode is None:
+                    flash("Invalid barcode, please try again")
+                    return redirect(url_for('search'))
+                gtin_upc = int(barcode.rjust(14, "0"))
+
+            resp = requests.get(f"https://wnpwytxwol.execute-api.us-east-1.amazonaws.com/v2/gtin_table?gtin_upc={gtin_upc}")
+            resp_json = resp.json()
+
+            if "Item" not in resp_json["body-json"].keys():
+                return render_template("pages/summary.html")
+
+            ingredients = resp_json["body-json"]["Item"]["ingredients"]["S"]
+            title = {
+                "owner": resp_json["body-json"]["Item"]["brand_owner"]["S"],
+                "name": resp_json["body-json"]["Item"]["brand_name"]["S"],
+                "subbrand": resp_json["body-json"]["Item"]["subbrand_name"]["S"],
+            }
 
             lang_model_resp = inference.get_lang_model_response(ingredients)
             ing_label_dict = json.loads(json.loads(lang_model_resp)['content'])
@@ -145,12 +163,13 @@ def search():
             # add to user history
             #FIXME find stringified json alternatives
             created_at = int(time.time())
-            labels_json = parse.quote(json.dumps(ing_label_dict).replace("\"", "'"), safe="")
+            labels_json = parse.quote(json.dumps(ing_label_dict), safe="")
             url = f"https://wnpwytxwol.execute-api.us-east-1.amazonaws.com/v2/history?user_email={session['email']}&created_at={created_at}&gtin_upc={gtin_upc}&stringified_labels_json={labels_json}"
             user_hist_resp = requests.post(url)
 
             # TODO labels of ingredients ENUM in inference and template
-            return render_template("pages/summary.html", ing_label_dict=ing_label_dict)
+            return render_template("pages/summary.html", ing_label_dict=ing_label_dict, title=title,
+                                   gtin_upc=gtin_upc)
 
     return render_template("forms/search.html", form=form)
 
--- flask-app/forms.py
+++ flask-app/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, HiddenField
+from wtforms.validators import Length, Regexp
 
 
 class GTINForm(FlaskForm):
+    barcode_val = StringField('barcode value', validators=[
+            Length(min=3, max=15),
+            Regexp("^[0-9]+$")
+    ])
     barcode_image = HiddenField('barcode')
